93f2cf094284aab88fdfdfefa1ccfc4469f3273e
Ronaldo Lopes <rlsanlo@gmail.com>
2023-09-04 23:51:36 -0300

    implemented the list tasks

---
+++ collections-java-api-2023/src/main/java/com/example/CarrinhoDeCompras.java
@@ -0,0 +1,58 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    List<Item> listaDeItens;
+
+    public CarrinhoDeCompras() {
+        this.listaDeItens = new ArrayList<>();
+    }
+
+    public void adicionarItem(Item item) {
+        listaDeItens.add(item);
+    }
+
+    public void removerItem(Item item) {
+        listaDeItens.remove(item);
+    }
+
+    public double calcularValorTotal(){
+        double valorTotal = 0;
+        for(Item item : listaDeItens){
+            valorTotal += item.getPreco() * item.getQuantidade();
+        }
+        return valorTotal;
+    }
+
+    public void exibirItens(){
+        for(Item item : listaDeItens){
+            System.out.println(item);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Item> listaDeItens = new ArrayList<>();
+        CarrinhoDeCompras carrinhoDeCompras = new CarrinhoDeCompras();
+        Item item1 = new Item("Arroz", 10.0, 2);
+        Item item2 = new Item("Feijão", 5.0, 3);
+        Item item3 = new Item("Macarrão", 3.0, 1);
+        Item item4 = new Item("Carne", 20.0, 2);
+
+        carrinhoDeCompras.adicionarItem(item1);
+        carrinhoDeCompras.adicionarItem(item2);
+        carrinhoDeCompras.adicionarItem(item3);
+        carrinhoDeCompras.adicionarItem(item4);
+
+        carrinhoDeCompras.exibirItens();
+        System.out.println("Valor total: " + carrinhoDeCompras.calcularValorTotal());
+
+        carrinhoDeCompras.removerItem(item1);
+
+        carrinhoDeCompras.exibirItens();
+        System.out.println("Valor total: " + carrinhoDeCompras.calcularValorTotal());
+    }
+
+
+}
---
+++ collections-java-api-2023/src/main/java/com/example/ListaTarefa.java
@@ -0,0 +1,82 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa(List<Tarefa> tarefaList) {
+        this.tarefaList = tarefaList;
+    }
+
+    public void adicionarTarefa(String descricao) {
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao) {
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for (Tarefa tarefa : tarefaList) {
+            if (tarefa.getDescricao().equalsIgnoreCase(descricao)) {
+                tarefasParaRemover.add(tarefa);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+
+    public int obterNumeroTotalDeTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricaoTarefas(){
+        for(Tarefa tarefa : tarefaList){
+            System.out.println(tarefa.getDescricao());
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Tarefa> tarefas = new ArrayList<>();
+        ListaTarefa listaTarefa = new ListaTarefa(tarefas);
+        listaTarefa.adicionarTarefa("Lavar o carro");
+        listaTarefa.adicionarTarefa("Lavar a louça");
+        listaTarefa.adicionarTarefa("Lavar a roupa");
+        listaTarefa.adicionarTarefa("Lavar a casa");
+        listaTarefa.adicionarTarefa("Lavar o quintal");
+        listaTarefa.adicionarTarefa("Lavar o banheiro");
+        listaTarefa.adicionarTarefa("Lavar a cozinha");
+        listaTarefa.adicionarTarefa("Lavar a sala");
+        listaTarefa.adicionarTarefa("Lavar o quarto");
+        listaTarefa.adicionarTarefa("Lavar a calçada");
+        listaTarefa.adicionarTarefa("Lavar a varanda");
+        listaTarefa.adicionarTarefa("Lavar o jardim");
+        listaTarefa.adicionarTarefa("Lavar o telhado");
+        listaTarefa.adicionarTarefa("Lavar o muro");
+        listaTarefa.adicionarTarefa("Lavar a piscina");
+        listaTarefa.adicionarTarefa("Lavar o carro");
+        listaTarefa.adicionarTarefa("Lavar a louça");
+        listaTarefa.adicionarTarefa("Lavar a roupa");
+        listaTarefa.adicionarTarefa("Lavar a casa");
+        listaTarefa.adicionarTarefa("Lavar o quintal");
+        listaTarefa.adicionarTarefa("Lavar o banheiro");
+        listaTarefa.adicionarTarefa("Lavar a cozinha");
+        listaTarefa.adicionarTarefa("Lavar a sala");
+        listaTarefa.adicionarTarefa("Lavar o quarto");
+        listaTarefa.adicionarTarefa("Lavar a calçada");
+        listaTarefa.adicionarTarefa("Lavar a varanda");
+        listaTarefa.adicionarTarefa("Lavar o jardim");
+        listaTarefa.adicionarTarefa("Lavar o telhado");
+        listaTarefa.adicionarTarefa("Lavar o muro");
+        listaTarefa.adicionarTarefa("Lavar a piscina");
+        listaTarefa.adicionarTarefa("Lavar o carro");
+
+        // testar remover tarefa
+        listaTarefa.removerTarefa("Lavar o carro");
+
+        // testar obter descricao tarefas
+        listaTarefa.obterDescricaoTarefas();
+
+        System.out.println("O numero total de tarefas é: " + listaTarefa.obterNumeroTotalDeTarefas());
+    }
+
+}
